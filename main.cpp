a74757ebf12a48ad2d4d8acc282ffb2f788c8c96
bezdarnosti <r403prod@gmail.com>
2025-09-21 11:58:27 +0500

    dev0.0.1

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+void init_imgui();
+void init_glfw();
+void loop();
+void shutdown();
+
+int main(int, char**){
+    std::cout << "Hello, from engine!\n";
+}
+
+void init_imgui() {
+
+}
+
+void init_glfw() {
+
+}
+
+void loop() {
+    
+}
+
+void shutdown() {
+
+}
