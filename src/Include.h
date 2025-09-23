b8bc445344d03016bc26cb6ebb241d53d291f10d
bezdarnosti <r403prod@gmail.com>
2025-09-24 01:14:07 +0500

    dev0.0.5

--- src/Include.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <glad/glad.h>
-#include <glfw/glfw3.h>
-#include <spdlog/spdlog.h>
-#include <string>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
